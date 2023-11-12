f7595ddb59a718a42d654aad15c1e407d5057f08
delmas <azertyazerty1ze@gmail.com>
2023-11-12 21:32:38 +0000

    header

--- contact.php
+++ contact.php
@@ -32,88 +32,96 @@
 <body>
   <div class="hero_area">
     <!-- header section strats -->
-    <header class="header_section">
-      <nav class="navbar navbar-expand-lg custom_nav-container ">
-        <a class="navbar-brand" href="index.php">
+      <header class="header_section">
+          <nav class="navbar navbar-expand-lg custom_nav-container ">
+              <a class="navbar-brand" href="index.php">
           <span>
             MPK-BOUTIK
           </span>
-        </a>
-        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-          <span class=""></span>
-        </button>
-
-        <div class="collapse navbar-collapse innerpage_navbar" id="navbarSupportedContent">
-          <ul class="navbar-nav  ">
-            <li class="nav-item ">
-              <a class="nav-link" href="index.php">acceuil<span class="sr-only">(current)</span></a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="bureau.php">
-                Bureau
               </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="clavier.php">
-                Clavier
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="cle.php">
-                Cle
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="fibre.php">
-                Fibre
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="ordinateur.php">
-                Ordinateur
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="serveur.php">
-                Serveur
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="souris.php">
-                Souris
-              </a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="imprimante.php">
-                Imprimante
-              </a>
-            </li>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="temoignage.php">
-                Temoignage
-              </a>
-            </li>
-          </ul>
-          <div class="user_option">
-            <a href="">
-              <i class="fa fa-user" aria-hidden="true"></i>
-              <span>
-                Login
-              </span>
-            </a>
-            <a href="">
-              <i class="fa fa-shopping-bag" aria-hidden="true"></i>
-            </a>
-            <form class="form-inline ">
-              <button class="btn nav_search-btn" type="submit">
-                <i class="fa fa-search" aria-hidden="true"></i>
+              <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                  <span class=""></span>
               </button>
-            </form>
-          </div>
-        </div>
-      </nav>
-    </header>
+
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                  <ul class="navbar-nav  ">
+                      <li class="nav-item active">
+                          <a class="nav-link" href="index.php">PAGE D'ACCEUIL <span class="sr-only">(current)</span></a>
+                      </li>
+
+                      <li class="nav-item">
+                          <a class="nav-link" href="bureau.php">
+                              Bureau
+                          </a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="clavier.php">
+                              Clavier
+                          </a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="cle.php">
+                              Cle
+                          </a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="fibre.php">
+                              Fibre
+                          </a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="ordinateur.php">
+                              Ordinateur
+                          </a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="serveur.php">
+                              Serveur
+                          </a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="souris.php">
+                              Souris
+                          </a>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="imprimante.php">
+                              Imprimante
+                          </a>
+                      </li>
+                      </li>
+                      <li class="nav-item">
+                          <a class="nav-link" href="temoignage.php">
+                              Temoignage
+                          </a>
+                      </li>
+
+                  </ul>
+                  <div class="user_option">
+                      <a href="connexion/index.php">
+                          <i class="fa fa-user" aria-hidden="true"></i>
+                          <span>
+                connexion
+              </span>
+                      </a>
+                      <a href="inscription/index.php">
+                          <i class="fa fa-user" aria-hidden="true"></i>
+                          <span>
+                inscription
+              </span>
+                      </a>
+                      <a href="">
+                          <i class="fa fa-shopping-bag" aria-hidden="true"></i>
+                      </a>
+                      <form class="form-inline ">
+                          <button class="btn nav_search-btn" type="submit">
+                              <i class="fa fa-search" aria-hidden="true"></i>
+                          </button>
+                      </form>
+                  </div>
+              </div>
+          </nav>
+      </header>
     <!-- end header section -->
 
   </div>
