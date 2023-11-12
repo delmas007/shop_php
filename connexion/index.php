f2f4b1bfe2e5971c8e8af8f1467c09bfb524e3bf
delmas <azertyazerty1ze@gmail.com>
2023-11-12 22:42:16 +0000

    header

--- connexion/index.php
+++ connexion/index.php
@@ -75,7 +75,8 @@ $mysqli->close();
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Connexion | CodingLab</title>
+    <link rel="shortcut icon" href="../images/favicon.png" type="image/x-icon">
+    <title>MPK-BOUTIK</title>
     <link rel="stylesheet" href="style.css">
 </head>
 
--- inscription/index.php
+++ inscription/index.php
@@ -43,7 +43,8 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
 <html lang="en" dir="ltr">
 <head>
     <meta charset="UTF-8">
-    <title>Responsive Registration Form | CodingLab</title>
+    <link rel="shortcut icon" href="../images/favicon.png" type="image/x-icon">
+    <title>MPK-BOUTIK</title>
     <link rel="stylesheet" href="style.css">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
