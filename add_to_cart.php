e2b3f1f9eedd4aaf7385d6bd3811f94639e57a54
delmas <azertyazerty1ze@gmail.com>
2023-11-15 23:34:26 +0000

    vente

--- add_to_cart.php
+++ add_to_cart.php
@@ -1,9 +1,11 @@
 <?php
+session_start();
 // Connexion à la base de données (assurez-vous de remplacer les valeurs par les vôtres)
 $servername = "localhost";
 $username = "root";
 $password = "";
 $dbname = "vente";
+$id = $_SESSION["id"];
 
 $conn = new mysqli($servername, $username, $password, $dbname);
 
@@ -17,7 +19,7 @@ $product_id = $_GET['product_id'];
 $product_type = $_GET['product_type'];
 
 // Exécuter la requête d'insertion dans la table de commandes (vous pouvez ajuster cela en fonction de votre modèle de données)
-$sql = "INSERT INTO commande (id_a, id_u) VALUES ('$product_id', '1')"; // Ici, '1' représente l'ID de l'utilisateur (à adapter)
+$sql = "INSERT INTO commande (id_a, id_u) VALUES ('$product_id', '$id')"; // Ici, '1' représente l'ID de l'utilisateur (à adapter)
 
 if ($conn->query($sql) === TRUE) {
     echo "Achat enregistré avec succès.";
